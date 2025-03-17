ae8c00b50375fdc15a722a6882c525012eceebe2
Sasukexo <aswinash1002@gmail.com>
2025-03-17 18:10:57 +0530

    made some changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent//components , directives , pipes
+    AppComponent,
+    InfoComponent//components , directives , pipes
   ],
   imports: [
     BrowserModule,
