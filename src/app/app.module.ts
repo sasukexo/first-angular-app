512a1c5507cca287df9f705bacc942fe5f0b95c9
Sasukexo <aswinash1002@gmail.com>
2025-03-18 16:28:29 +0530

    made changes to the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InfoComponent } from './info/info.component';
+import { AttributeExpoComponent } from './attribute-expo/attribute-expo.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InfoComponent//components , directives , pipes
+    AppComponent, 
+    InfoComponent,
+    AttributeExpoComponent//components , directives , pipes
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/attribute-expo/attribute-expo.component.ts
@@ -0,0 +1,68 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attribute-expo',
+  templateUrl: './attribute-expo.component.html',
+  styleUrl: './attribute-expo.component.css'
+})
+export class AttributeExpoComponent {
+  dataItems:string[]=["Furniture","Vegetables","Fruits","Goods"]
+  fontNames: string[] = [
+    "Arial", "Verdana", "Helvetica", "Tahoma", "Trebuchet MS", "Times New Roman", 
+    "Georgia", "Garamond", "Courier New", "Brush Script MT", "Impact", "Comic Sans MS", 
+    "Lucida Sans", "Lucida Console", "Palatino Linotype", "Segoe UI", "Candara", 
+    "Calibri", "Cambria", "Consolas", "Franklin Gothic Medium", "Gill Sans", 
+    "Century Gothic", "Arial Black", "Book Antiqua", "Copperplate", "Papyrus", 
+    "Perpetua", "Rockwell", "Symbol"
+  ]
+  fontname=this.fontNames[24]
+  theme1:any = {
+    backgroundColor: "#f0f8ff",
+    fontFamily: "Arial, sans-serif",
+    padding: "10px",
+    margin: "10px",
+    border: "1px solid black",
+    height: "300px",
+    width: "360px",
+
+  }
+
+  theme2:any = {
+    backgroundColor: "#faebd7",
+    fontFamily: "Verdana, sans-serif",
+    padding: "10px",
+    margin: "10px",
+    border: "1px solid black",
+    height: "300px",
+    width: "360px",
+  }
+
+  theme3:any = {
+    backgroundColor: "#ffd4e1",
+    fontFamily: "courier",
+    padding: "10px",
+    margin: "10px",
+    border: "1px solid #000",
+    height: "300px",
+    width: "360px",
+  }
+
+  currentTheme:any=this.theme1;
+
+  changeTheme(theme:string){
+    switch(theme) {
+      case 'theme1':
+      this.currentTheme = this.theme1;
+      break;
+      case 'theme2':
+      this.currentTheme = this.theme2;
+      break;
+      case 'theme3':
+      this.currentTheme = this.theme3;
+      break;
+      default:
+      this.currentTheme = this.theme1;
+    }
+  }
+}
